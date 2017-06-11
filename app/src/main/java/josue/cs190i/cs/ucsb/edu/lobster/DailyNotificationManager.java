58660b12432754ffb9f0b1a9470a6d39065e0329
Sean Shelton <seanshelton@umail.ucsb.edu>
2017-06-10 21:55:46 -0700

    Notification comes a day after today

--- app/src/main/java/josue/cs190i/cs/ucsb/edu/lobster/DailyNotificationManager.java
+++ app/src/main/java/josue/cs190i/cs/ucsb/edu/lobster/DailyNotificationManager.java
@@ -22,6 +22,8 @@ public class DailyNotificationManager {
 
     public void beginDailyNotifications() {
 
+        // calendar might no longer be necessary, unless you want to try having the notification
+        // come at a certain time.
         Calendar calendar = Calendar.getInstance();
         calendar.set(Calendar.HOUR_OF_DAY, 20);
         calendar.set(Calendar.MINUTE, 14);
@@ -32,7 +34,14 @@ public class DailyNotificationManager {
 
         AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
 
-        alarmManager.setRepeating(AlarmManager.ELAPSED_REALTIME_WAKEUP, SystemClock.elapsedRealtime(), AlarmManager.INTERVAL_DAY, pendingIntent);
+        // can set the interval to (one minute * 5) for demo purposes
+        //long one_minute = 60000;
+
+        // default start time: SystemClock.elapsedRealtime() + AlarmManager.INTERVAL_DAY (one day from right now)
+        // note: can set start time to calendar.getTimeInMillis() if experimenting with a certain time
+        alarmManager.setRepeating(AlarmManager.ELAPSED_REALTIME_WAKEUP, SystemClock.elapsedRealtime() + AlarmManager.INTERVAL_DAY, AlarmManager.INTERVAL_DAY, pendingIntent);
+
+        // needed to make notifications stop coming (e.g. if interval is too short, you'll get spammed)
         //alarmManager.cancel(pendingIntent);
     }
 
